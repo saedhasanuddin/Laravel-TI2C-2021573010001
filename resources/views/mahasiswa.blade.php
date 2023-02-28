830918779a3ad705558c3a3204fe4a834fc99213
saedhasnuddin <musaedhasanuddin5@gmail.com>
2023-02-28 11:10:31 +0700

    first commit

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ol>
+        <li><?php echo $mahasiswa01;?></li>
+        <li>Muhammad Rival</li>
+        <li>Rizki Ananda</li>
+        <li>Syahli kurniawan</li>
+    </ol>
+<div>
+Copyright &copy;<?php echo date ("Y");?>Program Studi Teknik Informatika-PNL
+</div>   
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return "Musaed HUHUH";
+});
+
+Route::get('/belajar',function(){
+    echo '<h1>Laravel</h1>';
+    return view ("tess");
+});
+
+route::get('/mahasiswa/profil/coba',function(){
+    echo '<h2 style = "text-align:center"><u> Welcome Profil Coba</u></h2>';
+});
+
+Route::get('/mahasiswa',function (){
+    return view('mahasiswa'.["mahasiswa01"=>"Musaed Hasanuddin"]);
+});
+
+Route::get('/',function (){
+    return view ('welcome');
+});
+
+Route::get('/hello',function(){
+    return'Hello word';
+});
+
+Route::get('/mahasiswa/{nama}',function($nama){
+    return"Tampilkan data mahasiswa bernama $nama";
+});
+
+Route::get('/stok_barang/{jenis}/{merek}', function ($jenis,$merek){
+    return"Cek sisa stok untuk $jenis $merek";
+});
+
+Route::get('/stok_barang/{jenis?}/{merek?}',function ($a = 'smartphone',$b='samsung'){
+    return "Cek sisa stok untuk $a $b";
+});
+
+Route::get('/user/{id}', function ($id){
+    return"tampilkan user dengan id =$id";
+})->where('id','[0-9]+');
+
+Route::get('/hubungi-kami',function(){
+    return'<h1>hubungi kami</h>';
+});
+route::redirect('/contact-us','/hubungi-kami');
